cf4b437abc9fab906d7e399a754b2a09919e5253
JustinSHong <jhong2009@gmail.com>
2018-10-27 20:17:06 -0700

    set up user reducer

---
+++ src/reducers/user.js
@@ -0,0 +1,15 @@
+// action types
+
+const initialState = {
+    email: "",
+    token: ""
+};
+
+const user = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default user;
