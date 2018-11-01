cc54fcd4789a72b8f0d3b2bcd378d5177066dbe1
JustinSHong <jhong2009@gmail.com>
2018-11-01 16:43:38 -0700

    set up CREATING_USER reducer case

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,21 @@
 // action types
+import {
+    CREATING_USER,
+    CREATE_USER,
+    CREATEUSER_ERROR
+} from "../actions/index.js";
 
 const initialState = {
     email: "",
-    token: ""
+    token: "",
+    authType: "",
+    status: ""
 };
 
 const user = (state = initialState, action) => {
     switch (action.type) {
+        case CREATING_USER:
+            return Object.assign({}, state, { status: "CREATING A NEW USER" });
         default:
             return state;
     }
