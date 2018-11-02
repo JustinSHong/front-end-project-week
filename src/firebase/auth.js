5294b2ec58f83a4cf52a291951e1e2cafb64e54c
JustinSHong <jhong2009@gmail.com>
2018-11-01 18:07:39 -0700

    defined sign up authentication function

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,1 +1,11 @@
 // interface between Firebase and this React app
+import { auth } from "./firebase";
+
+// Sign Up
+auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+    });
